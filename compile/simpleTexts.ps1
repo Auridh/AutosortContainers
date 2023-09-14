ed7e7644df69ff4d5b94b4a79d39c44a949cdb31
Auridh <git@auridh.net>
2023-09-14 11:56:23 +0200

    :construction: Hopefully fix simpleTexts addons forever

--- compile/simpleTexts.ps1
+++ compile/simpleTexts.ps1
@@ -1,6 +1,6 @@
 $modName = "AutosortContainers_SimpleTexts"
 $modHandle = "Auridh_AC_ST"
 $absPath = Resolve-Path -Path "./"
-divine -a convert-loca -g bg3 -s "$absPath/$modName/Localization/English/AutosortContainers.xml" -d "$absPath/$modName/Localization/English/AutosortContainers.loca";
+divine -a convert-loca -g bg3 -s "$absPath/$modName/Localization/English/AutosortContainers_SimpleTexts.xml" -d "$absPath/$modName/Localization/English/AutosortContainers_SimpleTexts.loca";
 divine -a create-package -g bg3 -s "$absPath/$modName" -d "$absPath/$modName.pak" -c lz4;
 Compress-Archive -Path "$absPath/$modName.pak" -DestinationPath "$absPath/$modName.zip" -Force
